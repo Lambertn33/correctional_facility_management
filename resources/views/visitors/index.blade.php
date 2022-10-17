50ca0e16b99a5bfdf2ca075a261131b9cc16fb3b
Lambertn33 <lambertgithub@gmail.com>
2022-10-17 11:31:50 +0200

    make video resizable to small devices

--- resources/views/visitors/index.blade.php
+++ resources/views/visitors/index.blade.php
@@ -1,7 +1,7 @@
 @extends('visitors.layouts')
 
 @section('content')
-<video controls autoplay loop style="width: 90vh">
+<video controls autoplay loop style="width: 100%;height:auto;">
     <source src="/assets/video/e-huza.mp4" type="video/mp4">
         Your browser does not support the video tag.
 </video>
