f5a90873289122b3ef80d09dfc2e5edf0736af97
Lambertn33 <lambertgithub@gmail.com>
2022-10-26 10:29:11 +0200

    adjust appointment time and date selection

--- resources/views/visitors/appointmentRequest.blade.php
+++ resources/views/visitors/appointmentRequest.blade.php
@@ -143,21 +143,16 @@
                 <input type="date"
                 class="form-control"
                 name="visitDate"
-                min="<?php echo date('Y-m-d')?>"
+                id="visitDate"
                 value="{{old('visitDate')}}"
                 required
                 >
             </div>
             <div class="col-md-4">
                 <label for="" class="form-label">Visit Time <span>*</span></label>
-                <input type="time"
-                class="form-control"
-                name="visitTime"
-                min="07:00" max="18:00" 
-                id="timeInput"
-                value="08:30"
-                required
-                >
+                <select class="form-select" name="visitTime" id="visitTime" required>
+                    <option selected disabled value="">Select time</option>
+                </select>
             </div>
             <div class="col-md-4">
                 <label for="inputPrison" class="form-label">Select Tariff <span>*</span></label>
@@ -181,5 +176,30 @@
 </form>
 @endsection
 <script>
-    //TODO SET MIN AND MAX TO TIME INPUT
+    window.onload = function() {
+        //TODO SET MIN AND MAX TO TIME INPUT
+        let allowedTimes = [
+            '08:30', '08:40', '08:50', '09:00', '09:10', '09:20',
+            '09:30', '09:40', '09:50', '10:00', '10:10', '10:20',
+            '10:30', '10:40', '10:50', '11:00', '11:10', '11:20',
+            '11:30', '11:40', '11:50', '12:00', '12:10', '12:20',
+            '12:30', '12:40', '12:50', '13:00', '13:10', '13:20',
+            '13:30', '13:40', '13:50', '14:00', '14:10', '14:20',
+            '14:30', '14:40', '14:50', '15:00', '15:10', '15:20', 
+            '15:30', '15:40', '15:50', '16:00', '16:10', '16:20',
+            '16:30'
+        ];
+        var selectInput = document.getElementById('visitTime');
+        for ( let i = 0; i <= allowedTimes.length; i++ ) {
+            if (allowedTimes[i] !== undefined) {
+                let option = document.createElement( 'option' );
+                option.value = option.text = allowedTimes[i];
+                selectInput.appendChild( option );
+            
+            }
+        }
+        let today = new Date();
+        let minDate = new Date(today.setDate(today.getDate() + 1)).toISOString().split("T")[0];
+        document.getElementById("visitDate").setAttribute('min', minDate);
+    }
 </script>
