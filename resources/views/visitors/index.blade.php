6261fff5c9896f78d8032bc1cde10c0e9a7a0935
Lambertn33 <lambertgithub@gmail.com>
2022-10-31 21:11:47 +0200

    change video

--- resources/views/visitors/index.blade.php
+++ resources/views/visitors/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <video controls autoplay loop style="width: 100%;height:auto;">
-    <source src="/assets/video/e-huza.mp4" type="video/mp4">
+    <source src="/assets/video/e-huza-video.mp4" type="video/mp4">
         Your browser does not support the video tag.
 </video>
 @endsection
