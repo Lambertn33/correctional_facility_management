b7358cfa7ce156a9042acd74b70bedf31e69684b
Lambertn33 <lambertgithub@gmail.com>
2023-06-08 10:27:09 +0200

    chore: clean

--- app/Http/Services/Common/SendMessage.php
+++ app/Http/Services/Common/SendMessage.php
@@ -1,15 +1,15 @@
-<?php 
+<?php
  namespace App\Http\Services\Common;
  use Illuminate\Support\Facades\Http;
 
  class SendMessage {
-    
+
     public function sendMessage($telephone, $message) {
         return Http::withHeaders([
             'Authorization' => 'Bearer '.env("SMS_TOKEN").'',
         ])->acceptJson()
         ->post(''.env("SMS_URL").'', [
-           'sender' => 'E-HUZA',
+           'sender' => 'CORRECTIONAL_FACILITY',
             'to' => '+'.$telephone,
             'text' => $message
         ]);
